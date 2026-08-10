1e8f6cb89c7b0cba82efa8bd23424dce180abb1c
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-08-10 14:37:32 +0300

    max sum in vector of vector using dp

--- recursive/maxsuminvectorofvector.cpp
+++ recursive/maxsuminvectorofvector.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 int n;
 vector<vector<int>>grid;
+vector<vector<int>>dp;
 int recur(int x,int y)
 {
+    if(dp[x][y]!=-1)
+     return dp[x][y];
     if(x==n-1 and y==n-1){
         return grid[x][y];
     }
@@ -15,8 +18,8 @@ int recur(int x,int y)
     
     if(x+1<n)
      ch2 = grid[x][y]+ recur(x+1,y);
-    
-    return max(ch,ch2);
+    dp[x][y]=max(ch,ch2);
+    return  dp[x][y];
 }
 
 
@@ -32,7 +35,7 @@ for(int i=0;i<n;i++)
       cin>>grid[i][j];
      }
  }
-
+dp=vector<vector<int>>(n,vector<int>(n,-1));
 int ans=recur(0,0);
 cout<<ans<<endl;
 
