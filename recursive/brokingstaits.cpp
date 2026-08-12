29ee6bd3627cff2805a51942a975733b1feeca70
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-08-12 21:04:03 +0300

    broking stairs

---
+++ recursive/brokingstaits.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+#include <bits/stdc++.h>
+int n;
+string s;
+vector<int>dp;
+
+int recur(int x)
+{  if(x>=n)
+   return 0;
+  if(x==n-1)
+    return 1;
+  if(s[x]=='#')
+   return 0;
+
+ 
+  if(dp[x]!=-1)
+    return dp[x]; 
+   int ch1=0;int ch2=0;int ch3=0;int ch4=0;int ch5=0;
+ if(x+1 < n and s[x+1]!='#')  
+   ch1=recur(x+1);
+ if(x+2 < n and s[x+2]!='#')  
+   ch2=recur(x+2); 
+  if(x+3 < n and s[x+3]!='#')  
+   ch3=recur(x+3);
+  if(x+4 < n and s[x+4]!='#')  
+   ch4=recur(x+4);
+
+   if(x+5 < n and s[x+5]!='#')  
+   ch5=recur(x+5);    
+ dp[x]=ch1+ch2+ch3+ch4+ch5;   
+return dp[x]; 
+
+}
+int main() {
+cin>>n;
+cin>>s;
+dp=vector<int>(n+1,-1);
+cout<<recur(0);
+
+    return 0;
+}
