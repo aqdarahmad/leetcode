4af59f23ed643c9aadb61e648664134b09dfdb74
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-08-21 01:26:44 +0300

    Recursive function to convert a decimal number to its binary representation.

--- recursive/bin.cpp
+++ recursive/bin.cpp
@@ -1,9 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Write a recursive function to convert a decimal number to its binary representation.
 
 void binary(int x)
 {
-    if(x==0)
-     return;
+    if (x == 0)
+        return;
 
-     binary(x/2);
-     cout<<x%2;
+    binary(x / 2);
+    cout << x % 2;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    binary(x);
+
+    return 0;
 }
